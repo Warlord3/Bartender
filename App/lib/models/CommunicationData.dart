8a539f3a5ded574aa22d2390243d3c7fcde9b92f
Jan-PC <traut.j@t-online.de>
2021-04-05 16:51:19 +0200

    Add Commands as classes

--- App/lib/models/CommunicationData.dart
+++ App/lib/models/CommunicationData.dart
@@ -1,3 +1,5 @@
+import 'package:bartender/models/Drinks.dart';
+
 class Command {
   String command;
   Command({this.command});
@@ -8,20 +10,18 @@ class Command {
   }
 }
 
-class Status extends Command {
+class Status {
   int numberPumpsRunning;
   int drinkID;
   int progress;
   List<PumpStatus> pumpStatus;
   Status({
-    String command,
     this.numberPumpsRunning,
     this.drinkID,
     this.progress,
     this.pumpStatus,
-  }) : super(command: command);
+  });
   Map<String, dynamic> toJson() => {
-        'command': command,
         'numberPumpsRunning': numberPumpsRunning,
         'drinkID': drinkID,
         'progress': progress,
@@ -30,17 +30,17 @@ class Status extends Command {
 
   factory Status.fromJson(Map<String, dynamic> parsedJson) {
     return new Status(
-        numberPumpsRunning: parsedJson['numberPumpsRunning'] == null
-            ? 0
-            : parsedJson['numberPumpsRunning'],
-        drinkID: parsedJson['drinkID'] == null ? -1 : parsedJson['drinkID'],
-        progress: parsedJson['progress'] == null ? -1 : parsedJson['progress'],
-        pumpStatus: parsedJson['pumpStatus'] == null
-            ? null
-            : (parsedJson['pumpStatus'] as List)
-                .map((i) => PumpStatus.fromJson(i))
-                .toList(),
-        command: parsedJson['command']);
+      numberPumpsRunning: parsedJson['numberPumpsRunning'] == null
+          ? 0
+          : parsedJson['numberPumpsRunning'],
+      drinkID: parsedJson['drinkID'] == null ? -1 : parsedJson['drinkID'],
+      progress: parsedJson['progress'] == null ? -1 : parsedJson['progress'],
+      pumpStatus: parsedJson['pumpStatus'] == null
+          ? null
+          : (parsedJson['pumpStatus'] as List)
+              .map((i) => PumpStatus.fromJson(i))
+              .toList(),
+    );
   }
 }
 
@@ -58,15 +58,149 @@ class PumpStatus {
       );
 }
 
-class Progress extends Command {
+class Progress {
   int progress;
   Progress({
-    String command,
     this.progress,
-  }) : super(command: command);
+  });
 
   factory Progress.fromJson(Map<String, dynamic> parsedJson) => Progress(
-        command: parsedJson['command'] == null ? "" : parsedJson['command'],
         progress: parsedJson['progress'] == null ? 0 : parsedJson['progress'],
+      );
+}
+
+class PumpConfiguration {
+  List<PumpConfig> configs = List<PumpConfig>.filled(
+      16,
+      PumpConfig(
+        beverageID: -1,
+        mlPerMinute: 0,
+      ));
+  PumpConfiguration({this.configs});
+  PumpConfiguration.testData() {
+    configs = List<PumpConfig>.generate(
+        16,
+        (index) => PumpConfig(
+              beverageID: index,
+              mlPerMinute: index * 10,
+            ));
+  }
+  bool get configurated {
+    return !configs
+        .any((element) => element.beverageID == -1 && element.mlPerMinute == 0);
+  }
+
+  void setBeverageID(int index, int id) {
+    configs[index].beverageID = id;
+  }
+
+  void setMlPerMinute(int index, int ml) {
+    configs[index].mlPerMinute = ml;
+  }
+
+  factory PumpConfiguration.fromJson(Map<String, dynamic> parsedJson) =>
+      PumpConfiguration(
+        configs: parsedJson['config'] == null
+            ? 0
+            : (parsedJson['config'] as List)
+                .map((i) => PumpConfig.fromJson(i))
+                .toList(),
+      );
+  Map<String, dynamic> toJson() => {
+        "command": "pump_config",
+        "config": configs.map((e) => e.toJson()).toList(),
+      };
+}
+
+class PumpConfig {
+  int beverageID;
+  int mlPerMinute;
+  PumpConfig({
+    this.beverageID,
+    this.mlPerMinute,
+  });
+  factory PumpConfig.fromJson(Map<String, dynamic> parsedJson) => PumpConfig(
+        beverageID:
+            parsedJson['beverageID'] == null ? -1 : parsedJson['beverageID'],
+        mlPerMinute:
+            parsedJson['mlPerMinute'] == null ? 0 : parsedJson['mlPerMinute'],
+      );
+  Map<String, dynamic> toJson() => {
+        "beverageID": beverageID,
+        "mlPerMinute": mlPerMinute,
+      };
+}
+
+class ConfigRequest {
+  final String command = "pump_config_request";
+  Map<String, dynamic> toJson() => {
+        'command': command,
+      };
+}
+
+class StartPump {
+  List<int> IDs;
+  StartPump({this.IDs});
+  Map<String, dynamic> toJson() => {
+        'command': "start_pump",
+        'IDs': IDs,
+      };
+}
+
+enum enPumpDirection {
+  stop,
+  forward,
+  backward,
+}
+
+class StartPumpAll {
+  enPumpDirection direction;
+  StartPumpAll({this.direction});
+  Map<String, dynamic> toJson() => {
+        'command': "start_pump_all",
+        'direction': direction,
+      };
+}
+
+class StopPump {
+  List<int> IDs;
+  StopPump({this.IDs});
+  Map<String, dynamic> toJson() => {
+        'command': "stop_pump",
+        'IDs': IDs,
+      };
+}
+
+class StopPumpAll {
+  Map<String, dynamic> toJson() => {
+        'command': "stop_pump_all",
+      };
+}
+
+class NewDrink {
+  Drink drink;
+  NewDrink({
+    this.drink,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "command": "new_drink",
+        "id": drink.id == null ? -1 : drink.id,
+        "ingredients": drink.ingredients == null
+            ? []
+            : (drink.ingredients.map((i) => i.toJsonAsCommand()).toList()),
+      };
+}
+
+class NewDrinkResponse {
+  bool accepted;
+  NewDrinkResponse({
+    this.accepted,
+  });
+
+  factory NewDrinkResponse.fromJson(Map<String, dynamic> parsedJson) =>
+      NewDrinkResponse(
+        accepted:
+            parsedJson['accepted'] == null ? false : parsedJson['accepted'],
       );
 }
