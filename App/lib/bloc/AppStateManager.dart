f0558c8a3cabb5d92404f04a815179c9c23e2ca8
Jan-PC <traut.j@t-online.de>
2021-06-11 09:04:49 +0200

    Add ProgressOverlay

--- App/lib/bloc/AppStateManager.dart
+++ App/lib/bloc/AppStateManager.dart
@@ -1,5 +1,10 @@
 import 'package:bartender/GlobalWidgets/NotifcationOverlay.dart';
+import 'package:bartender/bloc/DataManager.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'ThemeManager.dart';
 
 class AppStateManager {
   static GlobalKey<NavigatorState> keyNavigator = GlobalKey<NavigatorState>();
@@ -14,9 +19,15 @@ class AppStateManager {
   static bool pushedPage = false;
   static bool initIP = false;
   static bool initStorage = false;
+  static bool initConnection = false;
   static List<bool> showMoreInfo;
 
+  static Size screenSize = Size(0, 0);
+
   static OverlayEntry settingsOverlayEntry;
+  static bool settingsOverlayVisible = false;
+  static OverlayEntry progressOverlay;
+  static bool progressOverlayVisible = false;
 
   static void showOverlayEntry(String text, [NavigatorState navigator]) {
     OverlayEntry entry = OverlayEntry(builder: (BuildContext context) {
@@ -30,5 +41,57 @@ class AppStateManager {
     Future.delayed(Duration(seconds: 3), () {
       entry.remove();
     });
+  }
+
+  static void showProgressOverlay() {
+    if (!progressOverlayVisible) {
+      progressOverlayVisible = true;
+      progressOverlay = OverlayEntry(
+        builder: (buildContext) => Positioned(
+          bottom: 15,
+          left: 15,
+          child: Container(
+            height: 50,
+            width: screenSize.width - 30,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Theme.of(buildContext).backgroundColor),
+            child: Center(
+              child: Selector<DataManager, int>(
+                builder: (context, data, child) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: LinearProgressIndicator(
+                    value: data / 100,
+                  ),
+                ),
+                selector: (buildContext, dataManager) =>
+                    dataManager.drinkProgress,
+              ),
+            ),
+          ),
+        ),
+      );
+      if (progressOverlay == null) return;
+      keyNavigator.currentState.overlay.insert(progressOverlay);
+    }
+  }
+
+  static void removeProgressOverlay() {
+    if (progressOverlayVisible) {
+      progressOverlay?.remove();
+      progressOverlayVisible = false;
+    }
+  }
+
+  static void removeSettingsOverlay() {
+    if (settingsOverlayVisible) {
+      settingsOverlayEntry?.remove();
+      settingsOverlayVisible = false;
+    }
+  }
+
+  static void dispose() {
+    removeSettingsOverlay();
+    removeProgressOverlay();
   }
 }
