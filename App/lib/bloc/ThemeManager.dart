26f50e4343f4634237f59530be8334ca8e93f415
Jan-PC <traut.j@t-online.de>
2021-06-13 13:59:29 +0200

    Change Textstyle

--- App/lib/bloc/ThemeManager.dart
+++ App/lib/bloc/ThemeManager.dart
@@ -71,22 +71,66 @@ class Themes {
     // == Text
     textTheme: TextTheme(
       headline1: TextStyle(
-        fontSize: 55,
-        fontWeight: FontWeight.normal,
-        color: Colors.teal[400],
+        fontSize: 96,
+        fontWeight: FontWeight.w300,
+        letterSpacing: -1.5,
       ),
-      overline: TextStyle(
-        fontSize: 25,
-        color: Colors.teal[400],
+      headline2: TextStyle(
+        fontSize: 60,
+        fontWeight: FontWeight.w300,
+        letterSpacing: -0.5,
+      ),
+      headline3: TextStyle(
+        fontSize: 48,
+        fontWeight: FontWeight.w400,
+      ),
+      headline4: TextStyle(
+        fontSize: 34,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      headline5: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w400,
+      ),
+      headline6: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.normal,
       ),
       subtitle1: TextStyle(
-        fontSize: 18,
-        color: Colors.grey[900],
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.15,
+      ),
+      subtitle2: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.1,
       ),
       bodyText1: TextStyle(
-        fontSize: 18,
-        color: Colors.grey[900],
-        fontWeight: FontWeight.normal,
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.5,
+      ),
+      bodyText2: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      button: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 1.25,
+      ),
+      caption: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.4,
+      ),
+      overline: TextStyle(
+        fontSize: 10,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 1.5,
       ),
     ),
     // == Slider
@@ -167,22 +211,67 @@ class Themes {
     // == Text
     textTheme: TextTheme(
       headline1: TextStyle(
-        fontSize: 55,
-        color: Colors.cyan[400],
-        fontWeight: FontWeight.normal,
+        fontSize: 96,
+        fontWeight: FontWeight.w300,
+        letterSpacing: -1.5,
       ),
-      overline: TextStyle(
-        fontSize: 25,
-        color: Colors.cyan[400],
+      headline2: TextStyle(
+        fontSize: 60,
+        fontWeight: FontWeight.w300,
+        letterSpacing: -0.5,
+      ),
+      headline3: TextStyle(
+        fontSize: 48,
+        fontWeight: FontWeight.w400,
+      ),
+      headline4: TextStyle(
+        fontSize: 34,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      headline5: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w400,
+      ),
+      headline6: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.normal,
+        letterSpacing: 0.15,
       ),
       subtitle1: TextStyle(
-        fontSize: 18,
-        color: Colors.white,
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.15,
+      ),
+      subtitle2: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.1,
       ),
       bodyText1: TextStyle(
-        fontSize: 18,
-        color: Colors.white,
+        fontSize: 16,
         fontWeight: FontWeight.normal,
+        letterSpacing: 0.3,
+      ),
+      bodyText2: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.25,
+      ),
+      button: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w500,
+        letterSpacing: 0.5,
+      ),
+      caption: TextStyle(
+        fontSize: 12,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 0.4,
+      ),
+      overline: TextStyle(
+        fontSize: 10,
+        fontWeight: FontWeight.w400,
+        letterSpacing: 1.5,
       ),
     ),
 
